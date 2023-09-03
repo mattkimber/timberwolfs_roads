979a1551b812106924b92393d835e633afa4b6de
mattkimber <github@mattkimber.org.uk>
2023-09-03 19:54:53 +0100

    2x GUI support

--- build_gui.sh
+++ build_gui.sh
@@ -6,20 +6,27 @@ echo "Creating GUI"
 
 # GUI
 mkdir -p gui_rendered
+mkdir -p gui_rendered/1x
+mkdir -p gui_rendered/2x
 mkdir -p gui_output
+mkdir -p gui_output/1x
+mkdir -p gui_output/2x
 
 # Depot GUI
-../gorender/renderobject.exe -o gui_rendered/depot -8 -s 1 -m files/manifest_gui.json voxels/depot/empty/foster.vox 
+../gorender/renderobject.exe -o gui_rendered/1x/depot -8 -s 1 -m files/manifest_gui.json voxels/depot/empty/foster.vox 
+../gorender/renderobject.exe -o gui_rendered/2x/depot -8 -s 2 -m files/manifest_gui.json voxels/depot/empty/foster.vox 
 
 # Roads
 for i in voxels/crossroad/*.vox; do 
-    j=$(echo $i | cut -f3 -d/)
-    ../gorender/renderobject.exe -o gui_rendered/$j -8 -s 1 -m files/manifest_gui_crossroad.json $i
+    j=$(echo $i | cut -f3 -d/)z
+    ../gorender/renderobject.exe -o gui_rendered/1x/$j -8 -s 1 -m files/manifest_gui_crossroad.json $i
+    ../gorender/renderobject.exe -o gui_rendered/2x/$j -8 -s 2 -m files/manifest_gui_crossroad.json $i
 done  
 
 for i in voxels/straight/*.vox; do 
     j=$(echo $i | cut -f3 -d/)
-    ../gorender/renderobject.exe -o gui_rendered/$j -8 -s 1 -m files/manifest_gui_road.json $i
+    ../gorender/renderobject.exe -o gui_rendered/1x/$j -8 -s 1 -m files/manifest_gui_road.json $i
+    ../gorender/renderobject.exe -o gui_rendered/2x/$j -8 -s 2 -m files/manifest_gui_road.json $i
     k=$(echo $j | cut -f1 -d.)
     ./roadgoo/roadgoo.exe $k
 done  
--- roadgoo/cmd/roadgoo.go
+++ roadgoo/cmd/roadgoo.go
@@ -2,13 +2,13 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"image"
 	"image/color"
 	"image/draw"
 	"image/png"
 	"os"
 )
-
 
 type Flags struct {
 	Depot string
@@ -26,24 +26,26 @@ func main() {
 	for _, a := range flag.Args() {
 		// A little inefficient to do electric for everything, but
 		// we can live...
-		doIndexedImages(a, false)
-		doIndexedImages(a, true)
+		doIndexedImages(1, a, false)
+		doIndexedImages(1, a, true)
+		doIndexedImages(2, a, false)
+		doIndexedImages(2, a, true)
 	}
 }
 
-func setZero(dst *image.Paletted, x, y, w, h int) {
-	for i := 0; i < w; i++ {
-		for j := 0; j < h; j++ {
-			dst.SetColorIndex(i+x, j+y, 0)
+func setZero(scale int, dst *image.Paletted, x, y, w, h int) {
+	for i := 0; i < w*scale; i++ {
+		for j := 0; j < h*scale; j++ {
+			dst.SetColorIndex(i+(x*scale), j+(y*scale), 0)
 		}
 	}
 }
 
-func blit8bpp(dst, src *image.Paletted, x1, y1, x2, y2, w, h int) {
-	for i := 0; i < w; i++ {
-		for j := 0; j < h; j++ {
-			if src.ColorIndexAt(i+x2, j+y2) != 0 {
-				dst.SetColorIndex(i+x1, j+y1, src.ColorIndexAt(i+x2, j+y2))
+func blit8bpp(scale int, dst, src *image.Paletted, x1, y1, x2, y2, w, h int) {
+	for i := 0; i < w*scale; i++ {
+		for j := 0; j < h*scale; j++ {
+			if src.ColorIndexAt(i+(x2*scale), j+(y2*scale)) != 0 {
+				dst.SetColorIndex(i+(x1*scale), j+(y1*scale), src.ColorIndexAt(i+(x2*scale), j+(y2*scale)))
 			}
 		}
 	}
@@ -64,129 +66,127 @@ func loadImage(filename string) image.Image {
 	return img
 }
 
-func doIndexedImages(name string, elec bool) {
-	bounds := image.Rect(0, 0, 600, 34)
+func doIndexedImages(scale int, name string, elec bool) {
+	bounds := image.Rect(0, 0, 600*scale, 34*scale)
 
 	var pal color.Palette
-	roadImages := loadImage("gui_rendered/" + name + "_8bpp.png").(*image.Paletted)
+	roadImages := loadImage(fmt.Sprintf("gui_rendered/%dx/%s_8bpp.png", scale, name)).(*image.Paletted)
 	if p, ok := roadImages.ColorModel().(color.Palette); ok {
 		pal = p
 	}
 
-	spark := loadImage("gui_icons/spark_8bpp.png").(*image.Paletted)
+	spark := loadImage(fmt.Sprintf("gui_icons/%dx/spark_8bpp.png", scale)).(*image.Paletted)
 
 	// Create the output image
 	img := image.NewPaletted(bounds, pal)
 	draw.Draw(img, bounds, &image.Uniform{C: color.White}, image.Point{}, draw.Over)
 
 	// Draw 2x road
-	setZero(img, 0, 0, 20, 20)
-	blit8bpp(img, roadImages, 0, 3, 0, 0, 20, 14)
+	setZero(scale, img, 0, 0, 20, 20)
+	blit8bpp(scale, img, roadImages, 0, 3, 0, 0, 20, 14)
 	if elec {
-		blit8bpp(img, spark, 0+9, 8, 0, 0, 12, 12)
+		blit8bpp(scale, img, spark, 0+9, 8, 0, 0, 12, 12)
 	}
 
-
-	setZero(img, 25, 0, 20, 20)
-	blit8bpp(img, roadImages, 25, 3, 28, 0, 20, 14)
+	setZero(scale, img, 25, 0, 20, 20)
+	blit8bpp(scale, img, roadImages, 25, 3, 28, 0, 20, 14)
 	if elec {
-		blit8bpp(img, spark, 25+9, 8, 0, 0, 12, 12)
+		blit8bpp(scale, img, spark, 25+9, 8, 0, 0, 12, 12)
 	}
-
 
 	// Draw 1x crossroad
-	crossroad := loadImage("gui_rendered/" + name + "_crossroad_8bpp.png").(*image.Paletted)
-	setZero(img, 50, 0, 20, 20)
-	blit8bpp(img, crossroad, 50, 3, 0, 0, 20, 12)
+	crossroad := loadImage(fmt.Sprintf("gui_rendered/%dx/%s_crossroad_8bpp.png", scale, name)).(*image.Paletted)
+	setZero(scale, img, 50, 0, 20, 20)
+	blit8bpp(scale, img, crossroad, 50, 3, 0, 0, 20, 12)
 	if elec {
-		blit8bpp(img, spark, 50+9, 8, 0, 0, 12, 12)
+		blit8bpp(scale, img, spark, 50+9, 8, 0, 0, 12, 12)
 	}
 
 	// Draw 1x depot (depot overlaid on track)
-	depot := loadImage("gui_rendered/" + flags.Depot + "_8bpp.png").(*image.Paletted)
-	setZero(img, 75, 0, 20, 20)
-	blit8bpp(img, roadImages, 75, 5, 0, 0, 20, 14)
-	blit8bpp(img, depot, 75, 0, 11, 0, 20, 20)
+	depot := loadImage(fmt.Sprintf("gui_rendered/%dx/%s_8bpp.png", scale, flags.Depot)).(*image.Paletted)
+	setZero(scale, img, 75, 0, 20, 20)
+	blit8bpp(scale, img, roadImages, 75, 5, 0, 0, 20, 14)
+	blit8bpp(scale, img, depot, 75, 0, 11, 0, 20, 20)
 	if elec {
-		blit8bpp(img, spark, 75+9, 8, 0, 0, 12, 12)
+		blit8bpp(scale, img, spark, 75+9, 8, 0, 0, 12, 12)
 	}
 
 	// Draw 1x tunnel (tunnel overlaid on track)
-	tunnel := loadImage("gui_icons/tunnel_gui_8bpp.png").(*image.Paletted)
-	setZero(img, 100, 0, 20, 20)
-	blit8bpp(img, roadImages, 100, 4, 0, 0, 20, 14)
-	blit8bpp(img, tunnel, 100, 0, 0, 0, 20, 20)
+	tunnel := loadImage(fmt.Sprintf("gui_icons/%dx/tunnel_gui_8bpp.png", scale)).(*image.Paletted)
+	setZero(scale, img, 100, 0, 20, 20)
+	blit8bpp(scale, img, roadImages, 102, 4, 0, 0, 18, 14)
+	blit8bpp(scale, img, tunnel, 100, 0, 0, 0, 20, 20)
 	if elec {
-		blit8bpp(img, spark, 100+9, 8, 0, 0, 12, 12)
+		blit8bpp(scale, img, spark, 100+9, 8, 0, 0, 12, 12)
 	}
 
 	// Draw 1x convert (arrow overlaid on track)
-	convertArrow := loadImage("gui_icons/convert_icon_8bpp.png").(*image.Paletted)
-	setZero(img, 125, 0, 20, 20)
-	blit8bpp(img, roadImages, 125, 6, 56, 0, 20, 14)
-	blit8bpp(img, convertArrow, 125, 1, 0, 0, 20, 20)
+	convertArrow := loadImage(fmt.Sprintf("gui_icons/%dx/convert_icon_8bpp.png", scale)).(*image.Paletted)
+	setZero(scale, img, 125, 0, 20, 20)
+	blit8bpp(scale, img, roadImages, 125, 6, 56, 0, 20, 14)
+	blit8bpp(scale, img, convertArrow, 125, 1, 0, 0, 20, 20)
 	if elec {
-		blit8bpp(img, spark, 125+9, 8, 0, 0, 12, 12)
+		blit8bpp(scale, img, spark, 125+9, 8, 0, 0, 12, 12)
 	}
 
 	// Vertical (|) track
-	straightButton := loadImage("gui_icons/icon_straight_8bpp.png").(*image.Paletted)
-	crossingButton := loadImage("gui_icons/icon_crossroad_8bpp.png").(*image.Paletted)
+	straightButton := loadImage(fmt.Sprintf("gui_icons/%dx/icon_straight_8bpp.png", scale)).(*image.Paletted)
+	crossingButton := loadImage(fmt.Sprintf("gui_icons/%dx/icon_crossroad_8bpp.png", scale)).(*image.Paletted)
 
 	// Diagonal (/) road
-	setZero(img, 150, 0, 46, 28)
-	blit8bpp(img, straightButton, 150, 0, 0, 0, 46, 28)
-	blit8bpp(img, roadImages, 168, 10, 0, 0, 20, 14)
+	setZero(scale, img, 150, 0, 46, 28)
+	blit8bpp(scale, img, straightButton, 150, 0, 0, 0, 46, 28)
+	blit8bpp(scale, img, roadImages, 168, 10, 0, 0, 20, 14)
 	if elec {
-		blit8bpp(img, spark, 168+11, 10+3, 0, 0, 12, 12)
+		blit8bpp(scale, img, spark, 168+11, 10+3, 0, 0, 12, 12)
 	}
 
 	// Diagonal (\) road
-	setZero(img, 200, 0, 46, 28)
-	blit8bpp(img, straightButton, 200, 0, 0, 0, 46, 28)
-	blit8bpp(img, roadImages, 218, 10, 28, 0, 20, 14)
+	setZero(scale, img, 200, 0, 46, 28)
+	blit8bpp(scale, img, straightButton, 200, 0, 0, 0, 46, 28)
+	blit8bpp(scale, img, roadImages, 218, 10, 28, 0, 20, 14)
 	if elec {
-		blit8bpp(img, spark, 218+11, 10+3, 0, 0, 12, 12)
+		blit8bpp(scale, img, spark, 218+11, 10+3, 0, 0, 12, 12)
 	}
 	// Crossroad
-	setZero(img, 250, 0, 46, 28)
-	blit8bpp(img, straightButton, 250, 0, 0, 0,46, 28)
-	blit8bpp(img, crossroad, 268, 11, 0, 0, 20, 14)
+	setZero(scale, img, 250, 0, 46, 28)
+	blit8bpp(scale, img, straightButton, 250, 0, 0, 0, 46, 28)
+	blit8bpp(scale, img, crossroad, 268, 11, 0, 0, 20, 14)
 	if elec {
-		blit8bpp(img, spark, 268+11, 10+3, 0, 0, 12, 12)
+		blit8bpp(scale, img, spark, 268+11, 10+3, 0, 0, 12, 12)
 	}
 	// Depot
-	setZero(img, 300, 0, 54, 34)
-	blit8bpp(img, crossingButton, 300, 0, 0, 0, 54, 34)
-	blit8bpp(img, roadImages, 300+27, 5+12, 0, 0, 20, 14)
-	blit8bpp(img, depot, 300+16, 12, 0, 0, 31, 20)
+	setZero(scale, img, 300, 0, 54, 34)
+	blit8bpp(scale, img, crossingButton, 300, 0, 0, 0, 54, 34)
+	blit8bpp(scale, img, roadImages, 300+27, 5+12, 0, 0, 20, 14)
+	blit8bpp(scale, img, depot, 300+16, 12, 0, 0, 31, 20)
 	if elec {
-		blit8bpp(img, spark, 300+27+9, 10+8, 0, 0, 12, 12)
+		blit8bpp(scale, img, spark, 300+27+9, 10+8, 0, 0, 12, 12)
 	}
 
 	// Tunnel
-	setZero(img, 400, 0, 54, 34)
-	blit8bpp(img, crossingButton, 400, 0, 0, 0, 54, 34)
-	blit8bpp(img, roadImages, 424, 12+4, 0, 0, 20, 14)
-	blit8bpp(img, tunnel, 424, 12, 0, 0, 20, 20)
+	setZero(scale, img, 400, 0, 54, 34)
+	blit8bpp(scale, img, crossingButton, 400, 0, 0, 0, 54, 34)
+	blit8bpp(scale, img, roadImages, 424+2, 12+4, 0, 0, 20-2, 14)
+	blit8bpp(scale, img, tunnel, 424, 12, 0, 0, 20, 20)
 	if elec {
-		blit8bpp(img, spark, 424+9, 10+8, 0, 0, 12, 12)
+		blit8bpp(scale, img, spark, 424+9, 10+8, 0, 0, 12, 12)
 	}
 
 	// Convert
-	setZero(img, 500, 0, 54, 34)
-	blit8bpp(img, crossingButton, 500, 0, 0, 0, 54, 34)
-	blit8bpp(img, roadImages, 523, 12+4, 56, 0, 20, 14)
-	blit8bpp(img, convertArrow, 523, 12, 0, 0, 20, 20)
+	setZero(scale, img, 500, 0, 54, 34)
+	blit8bpp(scale, img, crossingButton, 500, 0, 0, 0, 54, 34)
+	blit8bpp(scale, img, roadImages, 523, 12+4, 56, 0, 20, 14)
+	blit8bpp(scale, img, convertArrow, 523, 12, 0, 0, 20, 20)
 	if elec {
-		blit8bpp(img, spark, 523+9, 10+8, 0, 0, 12, 12)
+		blit8bpp(scale, img, spark, 523+9, 10+8, 0, 0, 12, 12)
 	}
 
 	// Save the output image
 	if elec {
-		encode(img, "gui_output/gui_"+name+"_elec_8bpp.png")
+		encode(img, fmt.Sprintf("gui_output/%dx/gui_%s_elec_8bpp.png", scale, name))
 	} else {
-		encode(img, "gui_output/gui_"+name+"_8bpp.png")
+		encode(img, fmt.Sprintf("gui_output/%dx/gui_%s_8bpp.png", scale, name))
 	}
 }
 
